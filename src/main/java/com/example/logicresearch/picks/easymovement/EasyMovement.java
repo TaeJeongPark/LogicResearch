1ab36cd58b1faac39503c093bc2c10fbabfb24f4
TaeJeong <ksmpjc99@gmail.com>
2023-11-05 20:19:47 +0900

    Pick's 간편 이동 로직 연구

---
+++ src/main/java/com/example/logicresearch/picks/easymovement/EasyMovement.java
@@ -0,0 +1,115 @@
+package com.example.logicresearch.picks.easymovement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * packageName    : com.example.logicresearch.picks.easymovement
+ * fileName       : EasyMovement
+ * author         : TaeJeongPark
+ * date           : 2023-11-05
+ * description    : 간편 이동 로직 연구
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-11-05        TaeJeongPark       최초 생성
+ */
+public class EasyMovement {
+
+    public static void main(String[] args) {
+
+        // 기존의 배송지 목록
+        List<Map<String, Object>> shippingList = new ArrayList<>();
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 6);
+            put("sequence", 1);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 7);
+            put("sequence", 2);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 8);
+            put("sequence", 3);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 9);
+            put("sequence", 4);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 10);
+            put("sequence", 5);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 11);
+            put("sequence", 6);
+        }});
+
+        shippingList.add(new HashMap<String, Object>() {{
+            put("id", 12);
+            put("sequence", 7);
+        }});
+
+        System.out.println("==========> 기존 배송지 목록 : " + shippingList);
+
+        // 변경할 배송지 리스트
+        List<Map<String, Object>> changeList = new ArrayList<>();
+
+        changeList.add(new HashMap<String, Object>() {{
+            put("id", 6);
+            put("sequence", 2);
+        }});
+
+        changeList.add(new HashMap<String, Object>() {{
+            put("id", 8);
+            put("sequence", 3);
+        }});
+
+        changeList.add(new HashMap<String, Object>() {{
+            put("id", 10);
+            put("sequence", 4);
+        }});
+
+        System.out.println("==========> 변경할 배송지 리스트 : " + changeList);
+
+        final int changeNum = 4;   // 간편이동 할 번호
+        int changeNumCnt = changeNum - 1;   // 간편이동 할 번호
+        int maintainNum = 1; // 유지할 번호
+
+        // 배송지 순서 변경 로직
+        for(Map<String, Object> shippingListItem : shippingList) {
+            shippingListItem.replace("sequence", 0);
+        }
+        for(Map<String, Object> changeListItem : changeList) {
+            for(Map<String, Object> shippingListItem : shippingList) {
+                if(changeListItem.get("id").equals(shippingListItem.get("id"))) {
+                    shippingListItem.replace("sequence", changeNumCnt);
+                    changeNumCnt++;
+                }
+            }
+        }
+        for(Map<String, Object> shippingListItem : shippingList) {
+            if(shippingListItem.get("sequence").equals(0)) {
+                shippingListItem.replace("sequence", maintainNum);
+                maintainNum++;
+                if(maintainNum == changeNum) {
+                    maintainNum += (changeList.size() - 1);
+                }
+            }
+        }
+
+        System.out.println("==========> 변경된 배송지 목록 : " + shippingList);
+
+    }
+
+}
